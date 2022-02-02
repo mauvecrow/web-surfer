340c8d2d83cdb767ab409b172dcbfb624da08be2
Mauvecrow <64569782+mauvecrow@users.noreply.github.com>
2022-02-02 11:32:00 -0700

    Initial commit

---
+++ src/main/java/quangson/tech/websurfer/WebSurferApplication.java
@@ -0,0 +1,13 @@
+package quangson.tech.websurfer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebSurferApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebSurferApplication.class, args);
+    }
+
+}
---
+++ src/test/java/quangson/tech/websurfer/WebSurferApplicationTests.java
@@ -0,0 +1,13 @@
+package quangson.tech.websurfer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebSurferApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
